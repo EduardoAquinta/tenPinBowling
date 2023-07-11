ea6f70f298b07decd2ab748cd0a1962fb57b7bcb
Eduardo Aquinta <eddie.ashton@madetech.com>
2023-07-11 16:21:11 +0100

    reset as I want to try using frames

--- tenPinBowling.test.js
+++ tenPinBowling.test.js
@@ -31,14 +31,30 @@ describe("Unit tests for the ten pin bowling Kata", () => {
     })
     it("returns 29 if the input is 'X 45'", () => {
         let frames = "X 45";
-        expect(bowlingScore(frames)).toBe(29);
+        expect(bowlingScore(frames)).toBe(28);
     })
     it("returns 23 if the input is '1/ 45", () => {
         let frames = "1/ 45";
         expect(bowlingScore(frames)).toBe(23);
     })
-    it.only("returns 39 if the input is 'X X 45", () => {
+    it("returns 39 if the input is 'X X 45", () => {
         let frames = "X X 45";
-        expect(bowlingScore(frames)).toBe(39);
+        expect(bowlingScore(frames)).toBe(38);
+    })
+    it("returns if the input is '4/ 09 X 34 23 X 12 45 8/ 30'", () => {
+        let frames = "4/ 09 X 34 23 X 12 45 8/ 30";
+        expect(bowlingScore(frames)).toBe(89);
+    })
+    it("returns if the input is '43 62 01 X 8/ 70 00 63 9/ 53'", () => {
+        let frames = "43 62 01 X 8/ 70 00 63 9/ 53";
+        expect(bowlingScore(frames)).toBe(92);
+    })
+    it("returns 300 if the input is '9/ 90 36 53 X X 6/ 04 26 XXX'", () => {
+        let frames = "9/ 90 36 53 X X 6/ 04 26 XXX"
+        expect(bowlingScore(frames)).toBe(143);
+    })
+    it("returns 300 if the input is 'X X X X X X X X X X XXX'", () => {
+        let frames = "X X X X X X X X X XXX"
+            expect(bowlingScore(frames)).toBe(300);
     })
 })
